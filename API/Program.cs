6a11a707c69bd8221105329cd9eecbc90e29841b
Assir Abdukhalikov <asirushtomblack@gmail.com>
2023-05-05 19:51:38 +0600

    scaffold db

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+/*options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"])*/
+options.UseSqlServer(builder.Configuration.GetConnectionString("AsirushNtbook")));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
